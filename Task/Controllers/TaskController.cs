ffeb24f527a750b2e1ddc2ef47799b9f68e45c4d
Aluno <gustaledur4376@gmail.com>
2024-11-25 21:15:18 -0300

    PRIMEIRA COMITAÇÂO

---
+++ Task/Controllers/TaskController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using task.Models;
+
+namespace task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private static List<ModelTask> ModelTasks = new List<ModelTask>();
+
+        [HttpGet]
+        public ActionResult<List<ModelTask>> SearchTasks()
+        {
+            return Ok(ModelTasks);
+        }
+
+        [HttpPost]
+        public ActionResult<ModelTask> AddTask([FromBody] ModelTask newTask)
+        {
+            if (newTask == null)
+            {
+                return BadRequest("A tarefa não pode ser nula");
+            }
+
+            ModelTasks.Add(newTask);
+
+            return CreatedAtAction(nameof(SearchTasks), new { id = newTask.Id }, newTask);
+        }
+
+        [HttpDelete("{Id}")]
+        public ActionResult<ModelTask> RemoverTask(int Id)
+        {
+            var pesquisa = ModelTasks.Find(x => x.Id == Id);
+
+            if (pesquisa == null)
+                return NotFound("Tarefa não encontrada");
+
+            ModelTasks.Remove(pesquisa);
+
+            return Ok(pesquisa); // Retorna a tarefa removida
+        }
+    }
+}
---
+++ Task/Models/ModelTask.cs
@@ -0,0 +1,10 @@
+﻿namespace task.Models
+{
+    public class ModelTask
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string Id { get; set; } 
+    }
+}
